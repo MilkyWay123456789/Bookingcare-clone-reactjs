beac6610dc2f640c27336da726d1f3946c5bbf31
htcreater <tienph.19th@sv.dla.edu.vn>
2022-12-01 21:36:50 +0700

    route doctor

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,7 +5,6 @@ import { ConnectedRouter as Router } from 'connected-react-router';
 import { history } from '../redux'
 import { ToastContainer } from 'react-toastify';
 
-
 import { userIsAuthenticated, userIsNotAuthenticated } from '../hoc/authentication';
 
 import { path } from '../utils'
@@ -20,7 +19,8 @@ import { CustomToastCloseButton } from '../components/CustomToast';
 import ConfirmModal from '../components/ConfirmModal';
 import HomePage from './HomePage/HomePage.js';
 import CustomScrollbars from '../components/CustomScrollbars';
-import DetailDoctor from "../containers/Patient/Doctor/DetailDoctor"
+import DetailDoctor from "../containers/Patient/Doctor/DetailDoctor";
+import Doctor from '../routes/Doctor';
 
 class App extends Component {
 
@@ -49,13 +49,14 @@ class App extends Component {
                     <div className="main-container">
                         <ConfirmModal />
                         <div className="content-container">
-                            <CustomScrollbars style={{height:'100vh',width:'100%'}}>
+                            <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
                                 <Switch>
                                     <Route path={path.HOME} exact component={(Home)} />
                                     <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                     <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                    <Route path={"/doctor/"} component={userIsAuthenticated(Doctor)} />
                                     <Route path={path.HOMEPAGE} component={HomePage} />
-                                    <Route path={path.DETAIL_DOCTOR} component={DetailDoctor}/> 
+                                    <Route path={path.DETAIL_DOCTOR} component={DetailDoctor} />
                                 </Switch>
                             </CustomScrollbars>
                         </div>
@@ -68,16 +69,16 @@ class App extends Component {
                         /> */}
 
                         <ToastContainer
-                        position="bottom-right"
-                        autoClose={5000}
-                        hideProgressBar={false}
-                        newestOnTop={false}
-                        closeOnClick
-                        rtl={false}
-                        pauseOnFocusLoss
-                        draggable
-                        pauseOnHover
-                        theme="colored"
+                            position="bottom-right"
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                            theme="colored"
                         />
                     </div>
                 </Router>
--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -2,42 +2,58 @@ export const adminMenu = [
     { //Quản lí người dùng
         name: 'menu.admin.manage-user', menus: [
             {
-                name: 'menu.admin.crud',link: '/system/user-manage',
+                name: 'menu.admin.crud', link: '/system/user-manage',
             },
             {
-                name: 'menu.admin.crud-redux',link: '/system/user-redux',
+                name: 'menu.admin.crud-redux', link: '/system/user-redux',
             },
             {
-                name: 'menu.admin.manage-doctor',link: '/system/manage-doctor',
+                name: 'menu.admin.manage-doctor', link: '/system/manage-doctor',
                 // subMenus: [
                 //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
                 //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
                 // ]
             },
             {
-                name: 'menu.admin.manage-admin',link: '/system/manage-admin',
+                name: 'menu.admin.manage-admin', link: '/system/manage-admin',
             },
+            {
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule',
+            }
         ]
     },
     { //Quản lí phòng khám
         name: 'menu.admin.clinic', menus: [
             {
-                name: 'menu.admin.manage-clinic',link: '/system/mamage-clinic',
+                name: 'menu.admin.manage-clinic', link: '/system/mamage-clinic',
             },
         ]
     },
     { //Quản lí chuyên khoa
         name: 'menu.admin.specialty', menus: [
             {
-                name: 'menu.admin.manage-specialty',link: '/system/mamage-specialty',
+                name: 'menu.admin.manage-specialty', link: '/system/mamage-specialty',
             },
         ]
     },
     { //Quản lí bài đăng
         name: 'menu.admin.handbook', menus: [
             {
-                name: 'menu.admin.manage-handbook',link: '/system/mamage-handbook',
+                name: 'menu.admin.manage-handbook', link: '/system/mamage-handbook',
             },
         ]
     },
+];
+
+export const doctorMenu = [
+    {
+        name: 'menu.admin.manage-user',
+        menus: [
+            { //Quản lí doctor
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule',
+            },
+        ]
+
+    }
+
 ];
